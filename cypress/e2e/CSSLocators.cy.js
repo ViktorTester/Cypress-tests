2a91abc8a11b208f724e3eaf62bba37db2987cf3
ViktorK <viktorqawork@gmail.com>
2024-03-19 11:58:47 +0200

    minor improvements

--- cypress/e2e/CSSLocators.cy.js
+++ cypress/e2e/CSSLocators.cy.js
@@ -1,16 +1,16 @@
-//
+import * as loc from "./Locators/locators";
 
 describe("CSSLocators", () => {
 
     it("csslocators", () => {
 
-        cy.visit("http://atomationpractice.com/index.php")
-        cy.get("#search_query_top").type("T-Shirts")           //ID locator
+        cy.visit("http://automationpractice.com/index.php")
+        cy.get(loc.search_button).type("T-Shirts")           //ID locator
         // cy.get(".search_query").type("T-Shirts")                         //class locator
         // cy.get("[name='search_query']").type("T-Shirts")                 //attribute locator
         // cy.get(".search_query[name='search_query']").type("T-Shirts")    //class attribute locator
-        cy.get("[name='submit_search']").click                      //attribute locator
-        cy.get(".lighter").contains("T-Shirt")              //class locator
+        cy.get(loc.submit_button).click                      //attribute locator
+        cy.get(loc.common).contains("T-Shirt")              //class locator
 
         // переход на страницу
         // нахождение поиска и вставка туда значения
---
+++ cypress/e2e/Locators/locators.js
@@ -0,0 +1,3 @@
+export const search_button = ('#search_query_top')
+export const submit_button = ('[name="submit_search"]')
+export const common = ('.lighter')
