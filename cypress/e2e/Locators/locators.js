3494cbcb1375846482db8ba31a42c2f4c58e9520
ViktorTester <viktor345@inbox.lv>
2024-03-24 20:45:39 +0200

    locators.js updated

--- cypress/e2e/Locators/locators.js
+++ cypress/e2e/Locators/locators.js
@@ -1,3 +1,29 @@
-export const search_button = ('#search_query_top')
-export const submit_button = ('[name="submit_search"]')
-export const common = ('.lighter')
+export class automationpractice_locators {
+    static search_button = '#search_query_top';
+    static submit_button = '[name="submit_search"]';
+    static common = '.lighter';
+}
+
+export const url = "https://letcode.in/test"
+
+export class demo_qa {
+    // cookies
+    static cookies_consent = '.fc-button-label';
+
+    // radiobuttons
+    static radiobtns = '.card-footer-item[href="/radio"]';
+    static radio_yes = '#yes';
+    static radio_mb = '#maybe';
+    static radio_nobug = '#nobug';
+    static radio_no = '#no';
+
+    // checkboxes
+    static cbox = 'input[type="checkbox"]';
+    static all_cbox = 'label[class="checkbox"] > input[type="checkbox"]';
+
+    //dropdowns
+
+
+
+}
+
