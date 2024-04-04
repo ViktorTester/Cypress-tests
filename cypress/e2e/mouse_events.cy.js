52178807f8a4e4a442774a50d9285026c48608cc
ViktorTester <viktor345@inbox.lv>
2024-04-04 22:07:12 +0300

    mouse_events.cy.js updated loсators.js updated

--- cypress/e2e/mouse_events.cy.js
+++ cypress/e2e/mouse_events.cy.js
@@ -1,6 +1,7 @@
 import {demo_qa as dq} from "./Usefull/locators";
 import {urls as url} from "./Usefull/locators";
 import {table_texts as tt} from "./Usefull/texts_to_compare";
+
 describe('', () => {
     it('mouseover', () => {
 
@@ -33,7 +34,7 @@ describe('', () => {
         cy.get(dq.rightClickButton).rightclick();
     });
 
-    it.only('doubleclick', () => {
+    it('doubleclick', () => {
 
         cy.visit(url.url12)
 
@@ -42,18 +43,48 @@ describe('', () => {
 
         // так как элементы находятся во фрейме, нужно взаимодействовать с ним
         // запуск фрейма
-        cy.get(dq.frame).frameLoaded
+        cy.frameLoaded(dq.frame)
+
+        // проверка того, что второе поле пока пустое
+        cy.iframe(dq.frame).find(dq.secondTextField).should('be.empty')
+
+        // сделать даблклик по кнопке
         cy.iframe(dq.frame).find(dq.dblclickButton).trigger('dblclick')
-        //cy.iframe(dq.dblclickButton).trigger('dblclick')
 
+        // в поле ниже должно появиться значение из поля выше, делаем ассерт
+        cy.iframe(dq.frame).find(dq.secondTextField).should('have.value', 'Hello World!')
 
-        // // cy.get(dq.secondTextField).should('not.contain', 'Hello World!')
+        // второй вариант
+        cy.iframe(dq.frame).find(dq.dblclickButton).dblclick()
     });
 
     it('drag and drop using plugin', () => {
+        cy.visit(url.url13)
 
+        // выбрать элемент и указать куда перетащить его
+        // в данном случае Cypress ругался, что элемент не видит (хотя он видим),
+        // поэтому использовалась опция {force:true}
+        cy.get(dq.dragElementRome).drag(dq.dropElementItaly, {force: true})
     });
-    it('page scrolling', () => {
+
+    it.only('page scrolling', () => {
+
+        cy.visit(url.url14)
+
+        // указываем элемент до которого будет скролиться страница и команду
+        // необязательный элемент duration - сколько времени будет
+        // скролиться страница пока не дойдет до нужного элемента
+        // без него это произойдет мгновенно
+        cy.get(dq.educationID).scrollIntoView({duration: 5000})
+
+        // проверка того, что нужный элемент видим
+        // эту команду и верхнюю можно соединить в одну, так как у них один селектор
+        cy.get(dq.educationID).should('be.visible')
+
+
+        // теперь скроллим наверх и также проверяем, что элемент видим
+        cy.get(dq.latviaID).scrollIntoView({duration: 3000}).should('be.visible')
+
 
     });
 });
