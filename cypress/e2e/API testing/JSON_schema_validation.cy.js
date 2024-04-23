cbcc1fe958c235c251fe6fffab73912688d9f8f0
ViktorTester <viktor345@inbox.lv>
2024-04-23 23:23:36 +0300

    JSON_schema_validation.cy.js added

---
+++ cypress/e2e/API testing/JSON_schema_validation.cy.js
@@ -0,0 +1,29 @@
+import {urls as url} from "./API_usefull/Locators";
+import {schema} from "./API_usefull/Schemas";
+
+const Ajv = require('ajv');
+const ajv = new Ajv()
+
+describe('Schema validation', () => {
+
+    it('Schema response validation ', () => {
+
+        // обычный GET-запрос
+        cy.request({
+            method: 'GET',
+            url: url.API_url5
+            // как обычно переносим ответ в переменную
+        }).then((response) => {
+
+            // вносим в переменную нашу схему предварительно обработанную методом avj.compile()
+            const validate = ajv.compile(schema)
+            // вносим в переменную итог сравнения схемы и JSON-ответа на наш запрос
+            const isvalid = validate(response.body)
+            //делаем проверку на ответ true
+            expect(isvalid).to.be.true
+
+        })
+
+    });
+
+});
